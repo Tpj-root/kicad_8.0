ff01e308ff7b2671aa99991a312c493f26777b1d
Tpj-root <trichy_hackerspace@outlook.com>
2025-05-03 16:51:37 +0530

    And Here We Go

---
+++ Script/Bc_draw_line.py
@@ -0,0 +1,18 @@
+import pcbnew
+
+board = pcbnew.GetBoard()
+
+def mm(x, y):
+    return pcbnew.VECTOR2I(int(x * 1_000_000), int(y * 1_000_000))
+
+start = mm(10, 10)
+end = mm(50, 50)
+
+track = pcbnew.PCB_TRACK(board)
+track.SetStart(start)
+track.SetEnd(end)
+track.SetLayer(pcbnew.B_Cu)
+track.SetWidth(int(0.25 * 1_000_000))  # 0.25 mm
+
+board.Add(track)
+pcbnew.Refresh()
